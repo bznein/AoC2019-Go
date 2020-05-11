7baee99e103378e92103aa770e499695df3b0ea4
Nikolas De Giorgis <bznein@gmail.com>
2020-05-11 22:24:16 +0200

    Amend

--- Day02/main.go
+++ Day02/main.go
@@ -15,38 +15,6 @@ func check(e error) {
 	}
 }
 
-type IntCodeCompiler struct {
-	Values []int
-	Reg    int
-}
-
-func (i *IntCodeCompiler) run() {
-	for {
-		reg := i.Reg
-		switch opcode := i.Values[reg]; opcode {
-		case 1:
-			i.Values[i.Values[reg+3]] = i.Values[i.Values[reg+1]] + i.Values[i.Values[reg+2]]
-		case 2:
-			i.Values[i.Values[reg+3]] = i.Values[i.Values[reg+1]] * i.Values[i.Values[reg+2]]
-		case 99:
-			return
-		default:
-			panic("Wrong opcode!")
-		}
-		i.Reg += 4
-	}
-}
-
-func (c IntCodeCompiler) peek(pos int) int {
-	//TODO add bound check
-	return c.Values[pos]
-}
-
-func (c *IntCodeCompiler) Set(pos, val int) {
-	//TODO bound checks
-	c.Values[pos] = val
-}
-
 func main() {
 
 	f, err := os.Open("./input.txt")
